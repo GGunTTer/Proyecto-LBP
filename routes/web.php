547581f7c299a3d4793c043d5ce01d79e31efee2
GGunTTer <felipe.leiva2121@gmail.com>
2025-10-07 09:23:19 -0300

    Version Beta

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrinterController;
-use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
-use Mike42\Escpos\Printer;
-use Native\Laravel\Facades\Notification;
-use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\TestPrintController;
 
 Route::match(['GET','POST'], '/', [PrinterController::class, 'print']);
 
 
 Route::get('/test-print', TestPrintController::class, 'test-print');
-
-
 
 
